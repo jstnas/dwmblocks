12337864c8b107e19c7824ddc08b6b28f36fb1f2
Justinas Grigas <jstn_as@protonmail.com>
2021-04-24 23:45:48 +0100

    Changed default signal to 31
    Having the default signal set to 0 triggered other blocks

--- blocks.def.h
+++ blocks.def.h
@@ -2,18 +2,18 @@
 static const Block blocks[] = {
 	//Icon	Command										Update Interval	Update Signal
 	{"",	"/home/iota/.local/bin/blocks/music",		2,				1},
-	//{"",	"/home/iota/.local/bin/blocks/bluetooth",	10,				0},
-	{"",	"/home/iota/.local/bin/blocks/ip eth0",		10,				0},
-	{"",	"/home/iota/.local/bin/blocks/ip wlp5s0",	10,				0},
-	{"",	"/home/iota/.local/bin/blocks/packages",	0,				0},
+	//{"",	"/home/iota/.local/bin/blocks/bluetooth",	10,				31},
+	{"",	"/home/iota/.local/bin/blocks/ip eth0",		10,				31},
+	{"",	"/home/iota/.local/bin/blocks/ip wlp5s0",	10,				31},
+	{"",	"/home/iota/.local/bin/blocks/packages",	0,				31},
 	{"",	"/home/iota/.local/bin/blocks/rss",			60,				5},
-	{"",	"/home/iota/.local/bin/blocks/ram",			2,				0},
+	{"",	"/home/iota/.local/bin/blocks/ram",			2,				31},
 	{"",	"/home/iota/.local/bin/blocks/cpu",			2,				2},
-	{"",	"/home/iota/.local/bin/blocks/temperature",	5,				0},
+	{"",	"/home/iota/.local/bin/blocks/temperature",	5,				31},
 	//{"",	"/home/iota/.local/bin/blocks/backlight",	0,				3},
 	{"",	"/home/iota/.local/bin/blocks/volume",		0,				4},
-	//{"",	"/home/iota/.local/bin/blocks/battery",		10,				0},
-	{"",	"/home/iota/.local/bin/blocks/clock",		1,				0}
+	//{"",	"/home/iota/.local/bin/blocks/battery",		10,				31},
+	{"",	"/home/iota/.local/bin/blocks/clock",		1,				31}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
